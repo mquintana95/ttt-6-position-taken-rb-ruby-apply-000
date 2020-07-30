97e136ada02dc809687152c6f770c36e3620e67b
Marcus Quintana <mq97325@gmail.com>
2020-07-30 23:17:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,5 @@
 # code your #position_taken? method here!
-def position_taken?(board = [], indexc = 0)
+def position_taken?(board = [], index = 0)
   if '#position_taken?(" ")' == false
 end  
 
-position_taken([],8)
